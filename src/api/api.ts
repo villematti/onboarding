5e793238ec07e14d76ac4ef3143dc43e2bf59a9d
Ville-Matti Hakanpää <villematti@Ville-Mattis-MacBook-Pro.local>
2022-02-21 20:37:17 +0200

    Uncomment code

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { KEY_TASKS } from "../constants/constants";
 import { ITaskModel } from "../models/ITaskModel";
 import { IUserModel } from "../models/IUserModel";
 
@@ -9,16 +10,16 @@ export const fetchUsers = async () => {
 
 export const fetchTasks = async (userId: number) => {
     const { data } = await axios.get<ITaskModel[]>('https://jsonplaceholder.typicode.com/todos');
-    // let modifiedTasks: ITaskModel[] = [];
-    // const storedTasks = localStorage.getItem(KEY_TASKS);
-    // if (storedTasks) modifiedTasks = JSON.parse(storedTasks);
-    // return data.filter(t => t.userId === userId).map(x => {
-    //     let currentTask = x;
-    //     modifiedTasks.forEach(y => {
-    //         if (y.id === x.id) currentTask = y;
-    //     });
-    //     return currentTask;
-    // });
+    let modifiedTasks: ITaskModel[] = [];
+    const storedTasks = localStorage.getItem(KEY_TASKS);
+    if (storedTasks) modifiedTasks = JSON.parse(storedTasks);
+    return data.filter(t => t.userId === userId).map(x => {
+        let currentTask = x;
+        modifiedTasks.forEach(y => {
+            if (y.id === x.id) currentTask = y;
+        });
+        return currentTask;
+    });
     return data.filter(t => t.userId === userId);
 }
 
